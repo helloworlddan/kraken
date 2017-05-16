8ce6a9229656886ce423f2a75e078b5ef3979ed6
Daniel Stamer <daniel@stamer.info>
2017-05-16 21:49:56 +0200

    put in some interfaces
    Signed-off-by: Daniel Stamer <daniel@stamer.info>

---
+++ interfaces.go
@@ -0,0 +1,16 @@
+package kraken
+
+// Inspectable graph item
+type Inspectable interface {
+	Inspect()
+}
+
+// YamlSerializable graph item
+type YamlSerializable interface {
+	ToYaml()
+}
+
+// Sizable graph item computing rough memory size
+type Sizable interface {
+	Size()
+}
